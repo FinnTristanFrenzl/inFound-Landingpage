759f7fb4cb57c44bb113dcd373f2656d47fce0a3
Finn Frenzl <finnfrenzl14@gmail.com>
2025-06-18 10:09:31 +0200

    mvp 11

--- app/api/(mvp_routes)/fetch/route.ts
+++ app/api/(mvp_routes)/fetch/route.ts
@@ -5,11 +5,10 @@ import { fetchPosts } from "@/app/utils/fetchPosts"
 import filterPosts from "@/app/utils/filterPosts"
 import { formatPost } from "@/app/utils/formatPost"
 import { supabase } from "@/supabaseClient/supabase"
-import { unauthorized } from "next/navigation";
 import { NextResponse } from "next/server";
 
 
-export async function POST(req: Request, res: NextResponse) {
+export async function POST(req: Request) {
 
   if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({status: 401, text: 'unauthorized'})
